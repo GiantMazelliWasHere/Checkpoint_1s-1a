f5556b847710cf0c934136433effbf67dcd1d92e
eduardo <eduardo.mazelli@gmail.com>
2024-03-16 01:10:14 -0300

    Add files web front

---
+++ Checkpoint_02/filmes.js
@@ -0,0 +1,7 @@
+const my3FavoriteShows = ["A Clockwork Orange", "Nightmare Before Christmas", "Once Upon a Time in HollyWood"]
+
+if (my3FavoriteShows.length === 3){
+    console.log("True")
+} else {
+    console.log("False")
+}
